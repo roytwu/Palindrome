716c528de6d3319d3e16b77dd69fdc2b158c7409
roy <tsehuaiwu@gmail.com>
2018-06-23 20:34:09 -0700

    updating strSplit(), and changing its name to numOfPalinfrome() eventually

--- main.c
+++ main.c
@@ -1,60 +1,99 @@
-//File name: 
+//File name: main.c
 //Author: Roy T Wu
 //Goal: Given a sentence, tell how many palindromes in that sentence
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h> //bool
 
-//Determine if a string(word) is a palindrome
-void isPalindrome(char str[])
+//Determine if a string/word is a palindrome
+bool isPalindrome(char str[])
 {
+	//if the string is single char, not a palindrome
+	if(strlen(str) == 1) {return false;}
+
 	int leftm = 0; //left most of str
 	int rightm = strlen(str)-1; //right most of str
+
+	//if there is upper case in the string, turn it to lower case
+	for(int i=0; i<strlen(str); i++){
+		if(str[i]>64 && str[i]<91){
+			str[i]=str[i]+32;
+		}
+	}
 
 	while(leftm < rightm)
 	{
 		if(str[leftm++] != str[rightm--]){
-			printf("%s is not palindrome\n", str);
-			return;
+			//printf("%s is not palindrome\n", str);
+			return false;
 		}
 	}
-	printf("%s is palindrome\n", str);
+	//printf("%s is palindrome\n", str);
+	return true;
 }
 
-//split one sentence to individual words
-char * strSplit(char str[]) 
+
+int numOfPalindrome(char str[]) 
 {
 	char *p_token;
-	char *p_delimiter = " ,.-";
-	char *p_rtn[50];
+	char *p_delimiter = " ,.-~:!";
 	int i = 0;
+	int count = 0;
+	int boo;
 
+	//split string(a sentence) to tokes(individual words)
 	p_token = strtok(str, p_delimiter);
 	
 	while(p_token != NULL)
 	{
-		printf("%s\n", p_token);
-		//p_rtn[i] = malloc(40*sizeof(char));
-		p_rtn[i] = p_token;
+		boo = isPalindrome(p_token);
+		printf("%s: %d\n", p_token, boo);
+
+		if(boo == 1){
+			count++;	
+		}
+
 		p_token = strtok(NULL, p_delimiter);
 		i++;
 	}
-
-	return p_rtn;
+	return count;
 }
 
-
 int main(){
+	//isPalindrome("noon");
+	// isPalindrome("power");
 
-	isPalindrome("noon");
-	isPalindrome("power");
-
-	char strA[] = "This is a, sample string.";
-	strSplit(strA);
-
-	// printf("%s", strSplit(strA));
+	int num;
+	char strA[] = "Anna, the eagle eye, will leave at noon !";
+	num =numOfPalindrome(strA);
+	printf("Number of palindrome: %d\n", num);
 
 
 	return 0;
 }
+
+// void strSplit(char str[]) 
+// {
+// 	char *p_token;
+// 	char *p_delimiter = " ,.-~:";
+// 	int i = 0;
+// 	p_token = strtok(str, p_delimiter);	
+// 	while(p_token != NULL)
+// 	{
+// 		printf("%s\n", p_token);
+// 		p_token = strtok(NULL, p_delimiter);
+// 		i++;
+// 	}
+// }
+
+
+// unsigned int hash(char *str)
+// {
+// 	int sum =0;
+// 	for (int j=0; str[j]!='\0'; j++){
+// 		sum +=str[j];
+// 	}
+// 	return sum % HASH_MAX;
+// }
